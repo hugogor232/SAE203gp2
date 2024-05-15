d000772b1b683bd70b2eb8a435bbb36382e67b70
Le S <solaymane.elk@gmail.com>
2024-05-15 12:07:17 +0200

    Add files via upload

---
+++ ajout.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+  <?php  
+include 'functions.php';
+?>
+</head>
+<body>
+<?php  
+navbar();
+?>
+</body>
+</html>
+
