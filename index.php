f63170598635c1008551bdbf3cff99cd6c7e5df6
user <user@mRTe210158>
2024-05-16 09:58:13 +0200

    ajout des fichiers

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'functions.php';?>
+</head>
+<body>
+    <?php navbar(); ?>
+    <div class="container-fluid">
+    <table class="table table-striped table-hover">
+        <tr>
+            <th scope="col">Nom</th>
+            <th>Envoyé le</th>
+            <th>Envoyé par</th>
+        </tr>
+        <tr></tr>
+    </table>
+    </div>
+</body>
+</html>
