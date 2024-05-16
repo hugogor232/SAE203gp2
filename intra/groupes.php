65dfe72e7f3a4188ec48168644e66bbc3e424998
user <user@mRTe210158>
2024-05-16 11:55:41 +0200

    actu intra

---
+++ intra/groupes.php
@@ -0,0 +1,96 @@
+<?php session_start();  ?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+<?php
+    print_r($_SESSION);
+    include('functions.php');
+    genererHeader();
+    genererNavigation();
+    ?>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  
+    </head>
+    <body>
+    	<?php
+        echo '<div class="row">';
+    		$grpjson = json_decode(file_get_contents('grp.json', true));
+            foreach($grpjson as $grp) {
+            echo '
+                <div class="col-sm-2">
+                <div class="card" style="width: 15rem; height:15rem;">
+                  <div class="card-body">
+                    <h5 class="card-title">'.$grp->nom.'</h5>
+                    <p class="card-text">'.$grp->description.'</p>
+                    <a href="#" class="btn btn-primary">Gestion</a>
+                  </div>
+                </div>
+              </div>'
+
+                ;
+            }
+        echo '<div class="col-sm-2">
+                <div class="card" style="width: 15rem; height:15rem;">
+                  <div class="card-body">
+                    <h5 class="card-title"><b>Nouveau groupe</b></h5>
+                    <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Creer votre groupe</a>
+                  </div>
+                </div>
+              </div>';
+        echo '</div>';
+		?>
+
+</div>
+
+<div class="container">
+
+  <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+        <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Name</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nom du fichier">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Description</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Desciption">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+    </body>
+</html>
