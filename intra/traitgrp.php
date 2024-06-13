36c5065ad45e6924ee59a8a81c0eb2f63d685912
hugogor232 <hugogor232@gmail.com>
2024-06-13 21:36:47 +0200

    test

--- intra/traitgrp.php
+++ intra/traitgrp.php
@@ -1,10 +1,10 @@
 <?php 
 session_start(); //trait php
 $lecturejson = json_decode(file_get_contents("./data/groupes.json", true));
-$userjson = json_decode(file_get_contents('data/utilisateurs.json', true));
+$userjson = json_decode(file_get_contents('../Vitrine/data/users.json', true));
 //print_r($_POST);
 //var_dump($_POST);
-header('Location: groupes.php');
+//header('Location: groupes.php');
 $_SESSION["grpid"] = ($lecturejson[sizeof($lecturejson)-1]->id);
 echo($_SESSION["grpid"]);
 
